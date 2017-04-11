bc1a9c4c1df8a2a5eb0bfbf9269e8eb8e9e65830
Luca Tumedei <luca@theaveragedev.com>
2017-04-11 13:17:56 +0200

    fixed all develop merge issues

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitaf9bc18f025489619a91c21fb532d43e::getLoader();
+return ComposerAutoloaderInitfc434320822581b4659091269886c209::getLoader();
